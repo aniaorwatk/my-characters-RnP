a3ef887b799d2ef5d6307819892d1ac8b7138c14
nazwaaniaorwatk <orwat.ania@gmail.com>
2022-10-07 11:23:23 +0200

    create comp section

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,13 @@
 import React from 'react';
 import Header from './components/Header/Header';
+import Section from './components/Section/Section';
 
 
 function App() {
   return (
 <>
 <Header />
+<Section />
 </>
   );
 }
