1be2ec5eb8faeebda22db05192a789cb4aa0e7e6
nazwaaniaorwatk <orwat.ania@gmail.com>
2022-10-07 11:45:16 +0200

    add data and img

---
+++ src/data/dataCharacters.tsx
@@ -0,0 +1,33 @@
+export default [
+    {
+        id:1,
+        character:"IKRA NEMMONIS",
+        src:"gold.jpg",
+        game: "dnd",
+        death: false,
+        history:{
+            personality_traits: "Jest dobra, honorowa- nie zrobi nic co jest nie honorowe, dumna z klanu i swojego pochodzenia",
+            ideals: "Znaleźć Antoniego i Simoneona, Pomagać innym,Chce aby klan/ przodkowie byli z niej dumni",
+            bonds: "Balasar -  zmarły pradziadek, Patrin - zmarły brat, Antoni - młodzieniec, którego próbuje odnaleźć, Pietro - przyjaciel, połączył ich wspólny cel,Simoneon - handlarz niewolników, którego Ikra ściga.", 
+            flaws:"Ponad wszystko wierzy w swój klan i ich ideały/ tradycję. Po sytuacji z bratem przestrzega ich całkowicie, Bardzo dużo je, Jeśli coś zacznie, to musi to dokończyć.",
+            history: "xxxx",
+
+        },
+    },
+    // {
+    //     id:
+    //     character:
+    //     src:
+    //     game:
+    //     death:
+    //     history:{
+    //         personality_traits:
+    //         ideals:
+    //         bonds:
+    //         flaws:
+    //         history:
+
+    //     },
+    // },
+    
+]
