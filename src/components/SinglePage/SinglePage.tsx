ef41c11d3eabeb6baea0c108d2ea2377aca6d294
nazwaaniaorwatk <orwat.ania@gmail.com>
2022-11-28 13:48:31 +0100

    add style to SP

--- src/components/SinglePage/SinglePage.tsx
+++ src/components/SinglePage/SinglePage.tsx
@@ -1,5 +1,7 @@
 import { useParams } from "react-router-dom";
 import dataCharacters from "../../data/dataCharacters";
+import { labels } from "../../labels";
+import "./SinglePage.scss";
 
 const SinglePage = () => {
   const para = useParams();
@@ -7,24 +9,26 @@ const SinglePage = () => {
   const index: number = idCharacter as number;
   const dataAndIndex = dataCharacters[index];
   return (
-    <div>
-      <img src={dataAndIndex.src} alt={dataAndIndex.character} />
-      <div className="singlePage__data">
-        <p> Name Character: {dataAndIndex.character}</p>
-        <p> History</p>
-
+    <div className="singlePageBox">
+      <img className="singlePageBox__img"src={dataAndIndex.src} alt={dataAndIndex.character} />
+      <div className="singlePageBox__data">
+        <p className="singlePageBox__data-textCharacter">{dataAndIndex.character}</p>
         {dataAndIndex.game === "dnd" && (
-          <div>
-            <p>
-              Personality traits: {dataAndIndex.history.personality_traits}
+          <div  className="singlePageBox__data-textDnd">
+            <p >
+              <span className="singlePageBox__data-textDnd--title">{labels.singlePage.titlePersonalityTraits}</span> {dataAndIndex.history.personality_traits}
             </p>
-            <p> Ideals: {dataAndIndex.history.ideals}</p>
-            <p> Bonds: {dataAndIndex.history.bonds}</p>
-            <p> Flaws: {dataAndIndex.history.flaws}</p>
+            <p> <span className="singlePageBox__data-textDnd--title">{labels.singlePage.titleIdeals}</span> {dataAndIndex.history.ideals}</p>
+            <p> <span className="singlePageBox__data-textDnd--title">{labels.singlePage.titleBonds}</span> {dataAndIndex.history.bonds}</p>
+            <p> <span className="singlePageBox__data-textDnd--title">{labels.singlePage.titleFlaws}</span> {dataAndIndex.history.flaws}</p>
           </div>
         )}
 
-        <p>{dataAndIndex.history.history}</p>
+        <p className="singlePageBox__data-titleHistory"> {labels.singlePage.titleHistory}</p>
+
+
+
+        <p className="singlePageBox__data-history">{dataAndIndex.history.history}</p>
       </div>
     </div>
   );
