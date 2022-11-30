98133cff9e983dfe8784dabf781e34cd4f241f1b
nazwaaniaorwatk <orwat.ania@gmail.com>
2022-11-30 08:59:52 +0100

    add filter and button with filter

---
+++ src/components/Search.tsx
@@ -0,0 +1,19 @@
+
+interface ISearchType {
+    searchCharacter: (event: React.ChangeEvent<HTMLInputElement>)=> void;
+    query: string
+}
+
+const Search = ({ query, searchCharacter }: ISearchType) => {
+    return (
+        <input
+        className="inputSearch"
+        type="text"
+        value={query}
+        placeholder="Search..."
+        onChange={searchCharacter}
+      />
+    );
+  };
+  
+  export default Search;
--- src/components/Section/Section.tsx
+++ src/components/Section/Section.tsx
@@ -1,25 +1,72 @@
+import { Key, useEffect, useState } from "react";
 import dataCharacters from "../../data/dataCharacters";
 import Card from "../Card/Card";
+import Search from "../Search";
 import "./Section.scss";
 
+interface ISectionType{
+  death: boolean; 
+  game: string; 
+  id:  number; 
+  src: string; 
+  character: string; 
+  information: string;
+}
+
 const Section = () => {
-  const cards = dataCharacters.map((item) => {
-    return (
-      <Card
-      death={item.death}
-      game={item.game}
-      id={item.id}
-        key={item.id}
-        img={item.src}
-        characterName={item.character}
-        information={item.information}
-      />
-    );
+  const [query, setQuery] = useState<string>("");
+  const [f, setf]= useState<ISectionType[]>()
+
+
+
+  const searchCharacter = (event: React.ChangeEvent<HTMLInputElement>) => {
+    setQuery(event.target.value);
+    setf(filteredProducts)
+  };
+
+  const filteredProducts = dataCharacters.filter((character) => {
+    return character.character.toLowerCase().includes(query.toLowerCase());
+  });
+  const filterDnD = dataCharacters.filter((character) => {
+    return character.game.includes("dnd");
   });
 
-  return <div className="section">{cards} 
+  const filterC = dataCharacters.filter((character) => {
+    return character.game.includes("cthulhu");
+  });
 
-  </div>;
+  useEffect(()=>{
+    return setf(filteredProducts);
+  },[])
+
+
+let filterX = filteredProducts
+console.log(filterX)
+
+  let cards = (f || []).map((item) => {
+    {
+      return (
+        <Card
+          death={item.death}
+          game={item.game}
+          id={item.id}
+          key={item.id}
+          img={item.src}
+          characterName={item.character}
+          information={item.information}
+        />
+      );
+    }
+  });
+
+  return (
+    <>
+      <Search searchCharacter={searchCharacter} query={query} />
+      <button type="button" onClick={()=> setf(filterDnD) }>onlyDnD</button>
+      <button type="button" onClick={()=> setf(filterC)  }>onlyDnD</button>
+      <div className="section">{cards}</div>
+    </>
+  );
 };
 
 export default Section;
